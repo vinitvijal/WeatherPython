c78b4cb502911f3f6eab6c10339867336c442ca8
Vinit Kumar Vijal <vineet.shanker@soesector17rohini.co.in>
2021-07-22 00:44:59 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def weather(cityName):
-    apiID = "0c42f7f6b53b244c78a418f4f181282a"
+    apiID = "Your API Key"
     response = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiID)
     return response
 
